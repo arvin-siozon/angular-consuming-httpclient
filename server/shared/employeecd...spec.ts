5985a448ccae1079f225d293d12284af673ea60c
arvin-siozon <arvin_siozon@hotmail.com>
2020-08-16 23:26:14 +0800

    WIP

---
+++ server/shared/employeecd...spec.ts
@@ -0,0 +1,7 @@
+import { Employeecd.. } from './employeecd..';
+
+describe('Employeecd..', () => {
+  it('should create an instance', () => {
+    expect(new Employeecd..()).toBeTruthy();
+  });
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// HttpClient module for RESTful API
+import { HttpClientModule } from '@angular/common/http';
+
+// Routing module for router service
+import { AppRoutingModule } from './app-routing.module';
+
+import { EmployeeCreateComponent } from './employee-create/employee-create.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+
+// Forms module
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeCreateComponent,
+    EmployeeEditComponent,
+    EmployeeListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
